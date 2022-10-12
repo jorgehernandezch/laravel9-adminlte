965ea387691e651e15511ce06223f42a9fba1af7
Jorge Edo. Hernández <jorge@galpha.co>
2022-10-12 14:32:40 -0300

    Ajustes Page Dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('adminlte::page')
+@section('title', 'Dashboard')
+
+@section('css')
+
+@stop
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
